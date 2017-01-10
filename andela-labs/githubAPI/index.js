91bdf4375129d610ebf12b2767721ee72419263a
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-01-10 12:29:08 +0100

    removed comments

--- andela-labs/githubAPI/index.js
+++ andela-labs/githubAPI/index.js
@@ -1,26 +1,23 @@
 var GitHubApi = require("github");
 
 var github = new GitHubApi({
-    // optional
+    
     debug: true,
     protocol: "https",
-    host: "api.github.com", // should be api.github.com for GitHub
-    pathPrefix: "", // for some GHEs; none for GitHub
+    host: "api.github.com", 
+    pathPrefix: "", 
     headers: {
-        "user-agent": "rkterungwa16" // GitHub is happy with a unique user agent
+        "user-agent": "rkterungwa16" 
     },
     Promise: require('bluebird'),
-    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    followRedirects: false, 
     timeout: 5000
 });
 
-// TODO: optional authentication here depending on desired endpoints. See below in README.
+
 
 github.users.getFollowingForUser({
-    // optional
-    // headers: {
-    //     "cookie": "blahblah"
-    // },
+
     username: "jonathanegol"
 }, function(err, res) {
     console.log(JSON.stringify(res));
