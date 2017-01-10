428ab222dac0ff71a373c48fb34d816a322a8049
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-01-10 11:42:08 +0100

    reverseString function: first test fails

---
+++ andela-labs/reverseString/spec/librarySpec.js
@@ -0,0 +1,10 @@
+describe("Produce the reverse order of a word: ", function() {
+  describe("Case for en empty string", function() {
+
+    it("should return null for empty string", function() {
+      expect(reverseString('')).toEqual(null);
+    });
+
+  });
+
+});
