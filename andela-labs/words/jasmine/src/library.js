a97b7113d471130a8fdb815cc08e90a6fc95bcb1
Richard Terungwa Kombol <terungwakombol@gmail.com>
2017-01-10 03:38:45 +0100

    words function: first test passes

---
+++ andela-labs/words/jasmine/src/library.js
@@ -0,0 +1,18 @@
+function words (mString) {
+	var mObj = {};
+	var mArray = mString.split(/\W/);
+	for (var i=0; i<mArray.length; i++) {
+		if (mArray[i].length === 0) {
+			mObj;
+		}
+		else if (!(mArray[i] in mObj)) {
+
+			mObj[mArray[i]] = 1;
+		}
+
+		else {
+			mObj[mArray[i]] += 1;
+		}
+	}
+	return mObj;
+}
